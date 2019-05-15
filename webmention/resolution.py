6afa5ddc792fc5583fa341829fce2d90cd65e0fa
Benjamin Turner <benturn@gmail.com>
2019-05-14 22:39:45 -0700

    support resolving flatpages using flatpage middleware

--- webmention/resolution.py
+++ webmention/resolution.py
@@ -2,17 +2,24 @@ import requests
 
 from urllib.parse import urlparse
 
+from django.contrib.flatpages.views import flatpage
+from django.http import Http404
+
 try:
     from django.core.urlresolvers import resolve, Resolver404
 except ImportError:
     from django.urls import resolve, Resolver404
 
 
-def url_resolves(url):
+def url_resolves(url, request=None):
+    path = urlparse(url).path
     try:
-        resolve(urlparse(url).path)
+        resolve(path)
     except Resolver404:
-        return False
+        try:
+            flatpage(request, path)
+        except Http404:
+            return False
     return True
 
 
@@ -22,9 +29,9 @@ def fetch_and_validate_source(source, target):
         if target in str(response.content):
             return response.content
         else:
-            raise TargetNotFoundError('Source URL did not contain target URL')
+            raise TargetNotFoundError("Source URL did not contain target URL")
     else:
-        raise SourceFetchError('Could not fetch source URL')
+        raise SourceFetchError("Could not fetch source URL")
 
 
 class SourceFetchError(Exception):
