fbf60235a1cf0ed6c007338826e5f504fd81ab1f
Benjamin Turner <benturn@gmail.com>
2019-05-14 23:13:37 -0700

    Update urls.py

--- webmention/urls.py
+++ webmention/urls.py
@@ -7,7 +7,7 @@ app_name = "webmention"
 
 
 urlpatterns = [
-    url(r"^receive$", views.receive, name="receive"),
+    url(r"^$", views.receive, name="receive"),
     url(r"^send/$", views.SendWebMentionView.as_view(), name="send"),
     url(r"^submit/$", views.WebmentionCreateView.as_view(), name="submit"),
     url(
